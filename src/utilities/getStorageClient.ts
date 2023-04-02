b34482db77fd32d1c54629975ffc69552a42b5a7
Elliot DeNolf <denolfe@gmail.com>
2023-04-02 17:26:07 -0400

    chore: more logging

--- src/utilities/getStorageClient.ts
+++ src/utilities/getStorageClient.ts
@@ -14,55 +14,60 @@ let session: CognitoUserSession | null = null
 let identityID: string
 
 export const getStorageClient: GetStorageClient = async () => {
-  console.log({
-    PAYLOAD_CLOUD_BUCKET_REGION: process.env.PAYLOAD_CLOUD_BUCKET_REGION,
-    PAYLOAD_CLOUD_COGNITO_IDENTITY_POOL_ID: process.env.PAYLOAD_CLOUD_COGNITO_IDENTITY_POOL_ID,
-    PAYLOAD_CLOUD_COGNITO_USER_POOL_ID: process.env.PAYLOAD_CLOUD_COGNITO_USER_POOL_ID,
-    PAYLOAD_CLOUD_COGNITO_PASSWORD: process.env.PAYLOAD_CLOUD_COGNITO_PASSWORD,
-    PAYLOAD_CLOUD_PROJECT_ID: process.env.PAYLOAD_CLOUD_PROJECT_ID,
-    identityID,
-  })
+  try {
+    console.log({
+      PAYLOAD_CLOUD_BUCKET_REGION: process.env.PAYLOAD_CLOUD_BUCKET_REGION,
+      PAYLOAD_CLOUD_COGNITO_IDENTITY_POOL_ID: process.env.PAYLOAD_CLOUD_COGNITO_IDENTITY_POOL_ID,
+      PAYLOAD_CLOUD_COGNITO_USER_POOL_ID: process.env.PAYLOAD_CLOUD_COGNITO_USER_POOL_ID,
+      PAYLOAD_CLOUD_COGNITO_PASSWORD: process.env.PAYLOAD_CLOUD_COGNITO_PASSWORD,
+      PAYLOAD_CLOUD_PROJECT_ID: process.env.PAYLOAD_CLOUD_PROJECT_ID,
+      identityID,
+    })
 
-  if (storageClient && session?.isValid()) {
+    if (storageClient && session?.isValid()) {
+      return {
+        storageClient,
+        identityID,
+      }
+    }
+
+    session = await authAsCognitoUser(
+      process.env.PAYLOAD_CLOUD_PROJECT_ID as string,
+      process.env.PAYLOAD_CLOUD_COGNITO_PASSWORD as string,
+    )
+    console.log({ session })
+
+    const cognitoIdentity = new CognitoIdentityClient({
+      region: 'us-east-1',
+      credentials: fromCognitoIdentityPool({
+        identityPoolId: process.env.PAYLOAD_CLOUD_COGNITO_IDENTITY_POOL_ID as string,
+        logins: {
+          [`cognito-idp.us-east-1.amazonaws.com/${process.env.PAYLOAD_CLOUD_COGNITO_USER_POOL_ID}`]:
+            session.getIdToken().getJwtToken(),
+        },
+      }),
+    })
+    console.log({ cognitoIdentity })
+
+    const credentials = await cognitoIdentity.config.credentials()
+    console.log({ credentials })
+
+    // @ts-expect-error
+    identityID = credentials.identityId
+
+    storageClient = new AWS.S3({
+      region: process.env.PAYLOAD_CLOUD_BUCKET_REGION,
+      credentials,
+    })
+
+    console.log({ storageClient })
+
     return {
       storageClient,
       identityID,
     }
-  }
-
-  session = await authAsCognitoUser(
-    process.env.PAYLOAD_CLOUD_PROJECT_ID as string,
-    process.env.PAYLOAD_CLOUD_COGNITO_PASSWORD as string,
-  )
-  console.log({ session })
-
-  const cognitoIdentity = new CognitoIdentityClient({
-    region: 'us-east-1',
-    credentials: fromCognitoIdentityPool({
-      identityPoolId: process.env.PAYLOAD_CLOUD_COGNITO_IDENTITY_POOL_ID as string,
-      logins: {
-        [`cognito-idp.us-east-1.amazonaws.com/${process.env.PAYLOAD_CLOUD_COGNITO_USER_POOL_ID}`]:
-          session.getIdToken().getJwtToken(),
-      },
-    }),
-  })
-  console.log({ cognitoIdentity })
-
-  const credentials = await cognitoIdentity.config.credentials()
-  console.log({ credentials })
-
-  // @ts-expect-error
-  identityID = credentials.identityId
-
-  storageClient = new AWS.S3({
-    region: process.env.PAYLOAD_CLOUD_BUCKET_REGION,
-    credentials,
-  })
-
-  console.log({ storageClient })
-
-  return {
-    storageClient,
-    identityID,
+  } catch (error) {
+    console.log({ error })
+    throw error
   }
 }
